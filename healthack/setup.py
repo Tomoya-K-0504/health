f733d61392440087c0757ad85872d838d04da1cd
Tomoya Koike <makeffort134@gmail.com>
2018-10-08 18:17:01 +0900

    :update repo constructed

---
+++ healthack/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='health hack by analyzing health data',
+    author='Tomoya Koike',
+    license='',
+)
