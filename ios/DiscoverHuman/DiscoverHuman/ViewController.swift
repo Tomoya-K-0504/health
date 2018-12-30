55e29a446c945dd5c39d3395a1fb6d93022e2371
Tomoya Koike <makeffort134@gmail.com>
2018-12-31 07:28:23 +0900

    first commit

---
+++ ios/DiscoverHuman/DiscoverHuman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscoverHuman
+//
+//  Created by 小池智哉 on 2018/12/30.
+//  Copyright © 2018 小池智哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios/DiscoverHuman/DiscoverHumanTests/DiscoverHumanTests.swift
@@ -0,0 +1,34 @@
+//
+//  DiscoverHumanTests.swift
+//  DiscoverHumanTests
+//
+//  Created by 小池智哉 on 2018/12/30.
+//  Copyright © 2018 小池智哉. All rights reserved.
+//
+
+import XCTest
+@testable import DiscoverHuman
+
+class DiscoverHumanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
