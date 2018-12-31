7274b0aec095a00cc34402b0b1bac3df0498f1b0
Tomoya Koike <makeffort134@gmail.com>
2018-12-31 14:22:02 +0900

    first commit of sensors ios app

---
+++ ios/Sensors/Sensors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sensors
+//
+//  Created by 小池智哉 on 2018/12/31.
+//  Copyright © 2018 小池智哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios/Sensors/SensorsTests/SensorsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SensorsTests.swift
+//  SensorsTests
+//
+//  Created by 小池智哉 on 2018/12/31.
+//  Copyright © 2018 小池智哉. All rights reserved.
+//
+
+import XCTest
+@testable import Sensors
+
+class SensorsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
