d2baab20a1bdc2841ccb63bb28203ed332fa0afd
Tomoya Koike <makeffort134@gmail.com>
2019-01-02 23:19:19 +0900

    EnvSensor and ActSensor model save method done

--- docker-django/web/api_health/urls.py
+++ docker-django/web/api_health/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /polls/5/
     path('accel/', views.save_accel, name='save_accel'),
+    path('env/', views.save_env, name='save_env'),
+    path('act/', views.save_act, name='save_act'),
     # # ex: /polls/5/results/
     # path('<int:question_id>/results/', views.results, name='results'),
     # # ex: /polls/5/vote/
--- docker-django/web/api_health/views.py
+++ docker-django/web/api_health/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Acceleration, DataSource
+from .models import Acceleration, DataSource, EnvSensor, ActSensor
 
 
 def index(request):
@@ -16,5 +16,35 @@ def index(request):
 def save_accel(request):
     if request.method == "POST":
         accel_data = json.loads(request.body.decode("utf-8"))
-        acceleration = Acceleration.create(accel_data)
+        Acceleration.create(accel_data)
+    return HttpResponseRedirect(reverse('api:index'))
+
+
+@csrf_exempt
+def save_env(request):
+    if request.method == "POST":
+        env_data = json.loads(request.body.decode("utf-8"))
+        for env in env_data:
+            env_sensor = EnvSensor(
+                data_source=DataSource.objects.get(name=env["data_source"]),
+                brightness=env["values"]["brightness"],
+                m_peak_power=envenv["values"]["m_peak_power"],
+                m_average_power=envenv["values"]["m_average_power"]
+            )
+            env_sensor.save()
+    return HttpResponseRedirect(reverse('api:index'))
+
+
+@csrf_exempt
+def save_act(request):
+    if request.method == "POST":
+        act_data = json.loads(request.body.decode("utf-8"))
+        for act in env_data:
+            act_sensor = ActSensor(
+                data_source=DataSource.objects.get(name=act["data_source"]),
+                brightness=act["values"]["brightness"],
+                m_peak_power=act["values"]["m_peak_power"],
+                m_average_power=act["values"]["m_average_power"]
+            )
+            env_sensor.save()
     return HttpResponseRedirect(reverse('api:index'))
