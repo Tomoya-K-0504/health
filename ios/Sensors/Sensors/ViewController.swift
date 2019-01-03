dceaab1a0ee838d67920a850acd2885d0ff9c47b
Tomoya Koike <makeffort134@gmail.com>
2019-01-03 20:54:02 +0900

    comment out for debug modification

--- ios/Sensors/Sensors/ViewController.swift
+++ ios/Sensors/Sensors/ViewController.swift
@@ -30,21 +30,21 @@ private func postAccess(_ urlString: String, postString: String) {
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     request.httpMethod = "POST"
     request.httpBody = postString.data(using: .utf8)
-//    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//        guard let data = data, error == nil else {                                                 // check for fundamental networking error
-//            print("error=\(error)")
-//            return
-//        }
-//
-//        if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-//            print("statusCode should be 200, but is \(httpStatus.statusCode)")
-//            print("response = \(response)")
-//        }
-//
-//        let responseString = String(data: data, encoding: .utf8)
-//        print("responseString = \(responseString)")
-//    }
-//    task.resume()
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {                                                 // check for fundamental networking error
+            print("error=\(error)")
+            return
+        }
+
+        if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+            print("statusCode should be 200, but is \(httpStatus.statusCode)")
+            print("response = \(response)")
+        }
+
+        let responseString = String(data: data, encoding: .utf8)
+        print("responseString = \(responseString)")
+    }
+    task.resume()
 }
 
 
